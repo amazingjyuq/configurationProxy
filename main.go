a7e78c23bc02f7ee4b166ee222bafe66ef9907f5
quyunjiang <quyunjiang@jumpy.ai>
2023-06-08 21:03:32 +0800

    add test codes

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/amazingjyuq/configurationProxy/pkg"
 )
 
 func main() {
+	pkg.SetProxySettings("127.0.0.1:8001")
+	pkg.EnableProxySettings()
 	fmt.Println("yes")
 }
